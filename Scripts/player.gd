c561f629c70294f34072d3908f020eaa425a3c43
sinhah239 <sinhah239@gmail.com>
2025-10-31 21:04:49 -0600

    Added double jump. In-progress on wall-jump

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,20 +1,34 @@
 extends CharacterBody2D
 
 
-@export var walk_speed = 300.0
-@export var jump_speed = 400.0
-
+@export var walk_speed := 300.0
+@export var jump_speed := 400.0
+@export var number_of_jumps := 2
+ 
+var _debug := true
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		velocity.y = jump_speed * -1; 
+	
+	# Reset double jump.
+	if is_on_floor(): 
+		number_of_jumps = 2
 
-	# Get the input direction and handle the movement/deceleration.
+	# Handle ground jump.
+	if Input.is_action_just_pressed("move_jump") and (is_on_floor() or number_of_jumps > 0):
+		velocity.y = jump_speed * -1 
+		number_of_jumps -= 1
+
+	# Handle wall jump.
+	if Input.is_action_just_pressed("move_jump") and not is_on_floor() and is_on_wall(): 
+			velocity.y = jump_speed * -1
+			velocity.x = jump_speed * get_wall_normal().x
+	
+	# Get the input direction and handle the movement/deceleration.a
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
@@ -23,3 +37,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, walk_speed)
 
 	move_and_slide()
+	
+	if _debug: 
+		print("On Wall?: " + str(is_on_wall()))
+		print("Wall Normal: " + str(get_wall_normal()))
