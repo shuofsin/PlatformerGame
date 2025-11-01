32fd6a9740c06f90b0989e75a3a05fb86eee5b7b
sinhah239 <sinhah239@gmail.com>
2025-11-01 13:50:50 -0600

    Added wall dust. Still working on wall-slide and jump

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,17 +1,22 @@
 extends CharacterBody2D
 
+var _debug := true
 
 @export var walk_speed := 300.0
 @export var jump_speed := 400.0
 @export var number_of_jumps := 2
- 
-var _debug := true
+
+@onready var _wall_dust_right: GPUParticles2D = %WallDustRight
+@onready var _wall_dust_left: GPUParticles2D = %WallDustLeft
+
+const GRAVITY_NORMAL := 980.0
+const GRAVITY_WALL := 450.0
 
 func _physics_process(delta: float) -> void:
 
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y = GRAVITY_NORMAL * delta
 
 	
 	# Reset double jump.
@@ -23,11 +28,19 @@ func _physics_process(delta: float) -> void:
 		velocity.y = jump_speed * -1 
 		number_of_jumps -= 1
 
-	# Handle wall jump.
-	if Input.is_action_just_pressed("move_jump") and not is_on_floor() and is_on_wall(): 
-			velocity.y = jump_speed * -1
-			velocity.x = jump_speed * get_wall_normal().x
-	
+	# Wall normal vector points the oppisite direction of the wall
+	# Wall dust
+	if not is_on_floor() and velocity.y > 0:
+		_wall_dust_right.emitting = (is_on_wall() and get_wall_normal() == Vector2.LEFT)
+		_wall_dust_left.emitting = (is_on_wall() and get_wall_normal() == Vector2.RIGHT)
+	else: 
+		_wall_dust_right.emitting = false
+		_wall_dust_left.emitting = false
+		_gravity_multiplier = 1
+
+	# Simple wall slide
+	_gravity_multiplier = 0.5 if is_on_wall() else 1
+
 	# Get the input direction and handle the movement/deceleration.a
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -39,5 +52,4 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	if _debug: 
-		print("On Wall?: " + str(is_on_wall()))
-		print("Wall Normal: " + str(get_wall_normal()))
+		print("Gravity Multiplier: " + str(_gravity_multiplier))
