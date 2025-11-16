4f112ed5d910515d5d1017097d27e346f36bc6dc
shuofsin <sinhah239@gmail.com>
2025-11-15 18:12:54 -0700

    Added help text item

--- Scripts/Interactable/help_sign.gd
+++ Scripts/Interactable/help_sign.gd
@@ -8,21 +8,20 @@ class_name HelpSign
 @export_multiline var help_text: String = ""
 var has_been_read: bool = false
 
-func ready() -> void: 
+func _ready() -> void: 
 	has_been_read = false
 	text_box.text = ""
 	animations.play("hover_unread")
-	area_entered.connect(_display_text)
-	area_exited.connect(_remove_text)
+	body_entered.connect(_display_text)
+	body_exited.connect(_remove_text)
 
 func _display_text(_body_entered: CharacterBody2D) -> void: 
-	if (body_entered):
-		if has_been_read == false: 
-			has_been_read = true
-			animations.play("hover_read")
-		
-		text_box.text = help_text
+	text_box.text = help_text
 
 func _remove_text(_body_entered: CharacterBody2D) -> void: 
 	if (body_entered):
 		text_box.text = ""
+	
+	if has_been_read == false: 
+		has_been_read = true
+		animations.play("hover_read")
