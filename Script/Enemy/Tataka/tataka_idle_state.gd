d835283a24ace4768dd0168ca19807c8efbb0d0a
shuofsin <sinhah239@gmail.com>
2025-11-30 14:59:36 -0700

    Polishing up playtest

--- Script/Enemy/Tataka/tataka_idle_state.gd
+++ Script/Enemy/Tataka/tataka_idle_state.gd
@@ -6,6 +6,8 @@ func enter() -> void:
 	tataka.idle_timer = tataka.MAX_IDLE_TIME
 
 func physics_update(delta: float) -> void:
+	tataka.velocity.x = lerp(tataka.velocity.x, 0.0, tataka.x_velocity_weight)
+	
 	tataka.idle_timer -= delta 
 	if tataka.idle_timer <= 0:
 		transition.emit(self, "throw")
